8f28d4c5027162c6fd730a0d6b4c185418059b59
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-07-01 13:37:25 -0700

    Added documentation to Align.

--- src/core/geometry/align.rs
+++ src/core/geometry/align.rs
@@ -1,19 +1,31 @@
 
+/// Represents alignment along an axis within bounds.
 #[repr(u8)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Align {
+    /// Alignment at the minimum bound (left/top).
     #[default]
     Min = 0,
+    /// Alignment in the center.
     Center = 1,
+    /// Alignment at the maximum bound (right/bottom).
     Max = 2,
 }
 
 impl Align {
+    /// Alignment on the left.
     pub const LEFT: Self = Self::Min;
+    /// Alignment on the right.
     pub const RIGHT: Self = Self::Max;
+    /// Alignment at the top.
     pub const TOP: Self = Self::Min;
+    /// Alignment at the bottom.
     pub const BOTTOM: Self = Self::Max;
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_i8(align: i8) -> Self {
         match align {
@@ -23,6 +35,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_i16(align: i16) -> Self {
         match align {
@@ -32,6 +48,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_i32(align: i32) -> Self {
         match align {
@@ -41,6 +61,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_i64(align: i64) -> Self {
         match align {
@@ -50,6 +74,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_i128(align: i128) -> Self {
         match align {
@@ -59,6 +87,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_f32(align: f32) -> Self {
         if align < 0.0 {
@@ -70,6 +102,10 @@ impl Align {
         }
     }
 
+    /// Converts an integer value into an [Align].
+    /// `n < 0`  = [Align::Min]
+    /// `n == 0` = [Align::Center]
+    /// `n > 0`  = [Align::Max]
     #[inline]
     pub const fn from_f64(align: f64) -> Self {
         if align < 0.0 {
@@ -81,6 +117,7 @@ impl Align {
         }
     }
 
+    /// Align within `min` and `max`.
     #[inline]
     pub const fn align(self, min: f32, max: f32) -> f32 {
         match self {
