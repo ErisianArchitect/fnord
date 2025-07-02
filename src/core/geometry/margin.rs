6528bc3cd32b2d0fc1b09b8a63d738e3e85e7388
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-07-01 17:37:26 -0700

    Added margin.

---
+++ src/core/geometry/margin.rs
@@ -0,0 +1,122 @@
+
+#[repr(C)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Margin {
+    pub left: i8,
+    pub top: i8,
+    pub right: i8,
+    pub bottom: i8,
+}
+
+#[repr(C)]
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+pub struct Marginf {
+    pub left: f32,
+    pub top: f32,
+    pub right: f32,
+    pub bottom: f32,
+}
+
+impl Margin {
+    pub const ZERO: Self = Margin::same(0);
+    pub const NEG_ONE: Self = Margin::same(-1);
+    pub const S1: Self = Margin::same(1);
+    pub const S2: Self = Margin::same(2);
+    pub const S3: Self = Margin::same(3);
+    pub const S4: Self = Margin::same(4);
+    pub const S5: Self = Margin::same(5);
+    pub const S6: Self = Margin::same(6);
+    pub const S8: Self = Margin::same(8);
+    pub const S10: Self = Margin::same(10);
+    pub const S15: Self = Margin::same(15);
+    pub const S16: Self = Margin::same(16);
+    pub const S18: Self = Margin::same(18);
+    pub const S20: Self = Margin::same(20);
+    pub const S22: Self = Margin::same(22);
+    pub const S24: Self = Margin::same(24);
+    pub const S25: Self = Margin::same(25);
+    pub const S28: Self = Margin::same(28);
+    pub const S32: Self = Margin::same(32);
+    pub const S40: Self = Margin::same(40);
+    pub const S50: Self = Margin::same(50);
+    pub const S75: Self = Margin::same(75);
+    pub const S100: Self = Margin::same(100);
+    pub const MIN: Self = Margin::same(i8::MIN);
+    pub const MAX: Self = Margin::same(i8::MAX);
+
+    #[inline]
+    pub const fn new(left: i8, top: i8, right: i8, bottom: i8) -> Self {
+        Self {
+            left, top,
+            right, bottom,
+        }
+    }
+
+    #[inline]
+    pub const fn same(all: i8) -> Self {
+        Self {
+            left: all,
+            top: all,
+            right: all,
+            bottom: all,
+        }
+    }
+
+    #[inline]
+    pub const fn symmetric(x: i8, y: i8) -> Self {
+        Self {
+            left: x,
+            top: y,
+            right: x,
+            bottom: y,
+        }
+    }
+
+    #[inline]
+    pub const fn to_marginf(self) -> Marginf {
+        Marginf {
+            left: self.left as f32,
+            top: self.top as f32,
+            right: self.right as f32,
+            bottom: self.bottom as f32,
+        }
+    }
+
+    #[inline]
+    pub const fn add_margin(self, rhs: Margin) -> Self {
+        Self {
+            left: self.left + rhs.left,
+            top: self.top + rhs.top,
+            right: self.right + rhs.right,
+            bottom: self.bottom + rhs.bottom,
+        }
+    }
+
+    #[inline]
+    pub const fn sub_margin(self, rhs: Margin) -> Self {
+        Self {
+            left: self.left - rhs.left,
+            top: self.top - rhs.top,
+            right: self.right - rhs.right,
+            bottom: self.bottom - rhs.bottom,
+        }
+    }
+}
+
+impl std::ops::Add<Margin> for Margin {
+    type Output = Margin;
+
+    #[inline]
+    fn add(self, rhs: Margin) -> Self::Output {
+        self.add_margin(rhs)
+    }
+}
+
+impl std::ops::Sub<Margin> for Margin {
+    type Output = Margin;
+
+    #[inline]
+    fn sub(self, rhs: Margin) -> Self::Output {
+        self.sub_margin(rhs)
+    }
+}
