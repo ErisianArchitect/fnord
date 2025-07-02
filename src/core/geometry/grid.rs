eb16b24dbdd570751054d19b212dba948e7558cd
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-07-01 17:37:10 -0700

    Updated implementation.

--- src/core/geometry/grid.rs
+++ src/core/geometry/grid.rs
@@ -1,3 +1,5 @@
+use super::rect::Rect;
+
 use super::size::*;
 use super::pos::*;
 
@@ -14,12 +16,17 @@ pub const fn grid(offset: Pos, cell_size: Size) -> Grid {
 }
 
 impl Grid {
-    pub const UNIT: Self = Self::square(Pos::ZERO, 1.0);
+    pub const UNIT: Self = Self::square_origin(1.0);
     /// A unit grid (`width` and `height` are `1.0`) where the offset if half the size less than `(0.0, 0.0)`.
-    pub const UNIT_CENTERED: Self = Self::centered_square(Pos::ZERO, 1.0);
+    pub const UNIT_CENTERED: Self = Self::centered_square_origin(1.0);
     #[inline]
     pub const fn new(offset: Pos, cell_size: Size) -> Self {
         Self { offset, cell_size }
+    }
+
+    #[inline]
+    pub const fn new_origin(cell_size: Size) -> Self {
+        Self { offset: Pos::ZERO, cell_size }
     }
 
     #[inline]
@@ -29,14 +36,29 @@ impl Grid {
     }
 
     #[inline]
+    pub const fn centered_origin(cell_size: Size) -> Self {
+        Self::centered(Pos::ZERO, cell_size)
+    }
+
+    #[inline]
     pub const fn square(offset: Pos, size: f32) -> Self {
         Self::new(offset, Size::square(size))
+    }
+
+    #[inline]
+    pub const fn square_origin(size: f32) -> Self {
+        Self::square(Pos::ZERO, size)
     }
 
     #[inline]
     pub const fn centered_square(offset: Pos, size: f32) -> Self {
         let half = size * 0.5;
         Self::new(offset.sub_dims(half, half), Size::square(size))
+    }
+
+    #[inline]
+    pub const fn centered_square_origin(size: f32) -> Self {
+        Self::centered_square(Pos::ZERO, size)
     }
 
     #[inline]
@@ -68,6 +90,31 @@ impl Grid {
     pub fn snap_right_bottom(self, pos: Pos) -> Pos {
         let offset_pos = Pos::new(pos.x + self.cell_size.width, pos.y + self.cell_size.height);
         self.snap(offset_pos)
+    }
+
+    #[inline]
+    pub fn snap_center(self, pos: Pos) -> Pos {
+        let left_top = self.snap_left_top(pos);
+        let half_width = self.cell_size.mul_dims(0.5, 0.5);
+        left_top.add_dims(half_width.width, half_width.height)
+    }
+
+    #[inline]
+    /// Snaps the [Rect] so that it covers all of the grid cells that it intersects.
+    pub fn snap_rect(self, rect: Rect) -> Rect {
+        let min = self.snap_left_top(rect.min);
+        let max = self.snap_right_bottom(rect.max);
+        Rect {
+            min,
+            max,
+        }
+    }
+
+    /// Returns the [Rect] of the cell that `pos` is inside.
+    #[inline]
+    pub fn snap_cell_rect(self, pos: Pos) -> Rect {
+        let min = self.snap_left_top(pos);
+        Rect::from_min_size(min, self.cell_size)
     }
 }
 
