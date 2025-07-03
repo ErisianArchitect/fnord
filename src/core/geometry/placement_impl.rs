145c1513c3eba635ff25bad6e6154dc271c78524
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-07-02 21:35:56 -0700

    Added an implementation.

--- src/core/geometry/placement_impl.rs
+++ src/core/geometry/placement_impl.rs
@@ -6,3 +6,29 @@ pub enum Placement {
     Middle = 1,
     Outside = 2,
 }
+
+impl Placement {
+    #[inline]
+    pub const fn invert(self) -> Self {
+        match self {
+            Placement::Inside => Placement::Outside,
+            Placement::Middle => Placement::Middle,
+            Placement::Outside => Placement::Inside,
+        }
+    }
+
+    #[inline]
+    pub const fn is_inside(self) -> bool {
+        matches!(self, Placement::Inside)
+    }
+
+    #[inline]
+    pub const fn is_middle(self) -> bool {
+        matches!(self, Placement::Middle)
+    }
+
+    #[inline]
+    pub const fn is_outside(self) -> bool {
+        matches!(self, Placement::Outside)
+    }
+}
