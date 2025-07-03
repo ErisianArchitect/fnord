6dfd5635cdd3cdce6e3b6c769fa709214e89c2ff
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-07-02 21:33:56 -0700

    Added more functions.

--- src/core/geometry/util_impl.rs
+++ src/core/geometry/util_impl.rs
@@ -3,3 +3,23 @@
 pub const fn half(value: f32) -> f32 {
     value * 0.5
 }
+
+#[inline]
+pub const fn quarter(value: f32) -> f32 {
+    value * 0.25
+}
+
+#[inline]
+pub const fn third(value: f32) -> f32 {
+    value * 0.3333333333333333
+}
+
+#[inline]
+pub const fn fifth(value: f32) -> f32 {
+    value * 0.2
+}
+
+#[inline]
+pub const fn tenth(value: f32) -> f32 {
+    value * 0.1
+}
