a576cf27c5dce91447f441d33525c9f763753184
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-11-18 16:48:03 -0800

    Attributes and local_to_world/world_to_local

--- src/core/geometry/grid_impl.rs
+++ src/core/geometry/grid_impl.rs
@@ -11,6 +11,7 @@ pub struct Grid {
 }
 
 #[inline]
+#[must_use]
 pub const fn grid(offset: Pos, cell_size: Size) -> Grid {
     Grid { offset, cell_size }
 }
@@ -20,54 +21,64 @@ impl Grid {
     /// A unit grid (`width` and `height` are `1.0`) where the offset if half the size less than `(0.0, 0.0)`.
     pub const UNIT_CENTERED: Self = Self::centered_square_origin(1.0);
     #[inline]
+    #[must_use]
     pub const fn new(offset: Pos, cell_size: Size) -> Self {
         Self { offset, cell_size }
     }
 
     #[inline]
+    #[must_use]
     pub const fn new_origin(cell_size: Size) -> Self {
         Self { offset: Pos::ZERO, cell_size }
     }
 
     #[inline]
+    #[must_use]
     pub const fn centered(center: Pos, cell_size: Size) -> Self {
         let half_size = cell_size.mul_dims(0.5, 0.5);
         Self::new(center.sub_dims(half_size.width, half_size.height), cell_size)
     }
 
     #[inline]
+    #[must_use]
     pub const fn centered_origin(cell_size: Size) -> Self {
         Self::centered(Pos::ZERO, cell_size)
     }
 
     #[inline]
+    #[must_use]
     pub const fn square(offset: Pos, size: f32) -> Self {
         Self::new(offset, Size::square(size))
     }
 
     #[inline]
+    #[must_use]
     pub const fn square_origin(size: f32) -> Self {
         Self::square(Pos::ZERO, size)
     }
 
     #[inline]
+    #[must_use]
     pub const fn centered_square(offset: Pos, size: f32) -> Self {
         let half = size * 0.5;
         Self::new(offset.sub_dims(half, half), Size::square(size))
     }
 
     #[inline]
+    #[must_use]
     pub const fn centered_square_origin(size: f32) -> Self {
         Self::centered_square(Pos::ZERO, size)
     }
 
     /// Creates a grid where the given [Rect] becomes the cell at `(0, 0)`.
     #[inline]
+    #[must_use]
     pub const fn from_rect(rect: Rect) -> Self {
         Self::new(rect.min, rect.size())
     }
 
     #[inline]
+    #[must_use]
     pub fn snap(self, pos: Pos) -> Pos {
         let offset_pos = pos.sub_dims(self.offset.x, self.offset.y);
         let offset_pos_rem = offset_pos.rem_euclid_dims(self.cell_size.width, self.cell_size.height);
@@ -76,37 +87,43 @@ impl Grid {
     }
 
     #[inline]
+    #[must_use]
     pub fn snap_left_top(self, pos: Pos) -> Pos {
         self.snap(pos)
     }
 
     #[inline]
+    #[must_use]
     pub fn snap_right_top(self, pos: Pos) -> Pos {
         let offset_pos = Pos::new(pos.x + self.cell_size.width, pos.y);
         self.snap(offset_pos)
     }
 
     #[inline]
+    #[must_use]
     pub fn snap_left_bottom(self, pos: Pos) -> Pos {
         let offset_pos = Pos::new(pos.x, pos.y + self.cell_size.height);
         self.snap(offset_pos)
     }
 
     #[inline]
+    #[must_use]
     pub fn snap_right_bottom(self, pos: Pos) -> Pos {
         let offset_pos = Pos::new(pos.x + self.cell_size.width, pos.y + self.cell_size.height);
         self.snap(offset_pos)
     }
 
     #[inline]
+    #[must_use]
     pub fn snap_center(self, pos: Pos) -> Pos {
         let left_top = self.snap_left_top(pos);
         let half_width = self.cell_size.mul_dims(0.5, 0.5);
         left_top.add_dims(half_width.width, half_width.height)
     }
 
-    #[inline]
     /// Snaps the [Rect] so that it covers all of the grid cells that it intersects.
+    #[inline]
+    #[must_use]
     pub fn snap_rect(self, rect: Rect) -> Rect {
         let min = self.snap_left_top(rect.min);
         let max = self.snap_right_bottom(rect.max);
@@ -118,12 +135,14 @@ impl Grid {
 
     /// Returns the [Rect] of the cell that `pos` is inside.
     #[inline]
+    #[must_use]
     pub fn snap_cell_rect(self, pos: Pos) -> Rect {
         let min = self.snap_left_top(pos);
         Rect::from_min_size(min, self.cell_size)
     }
 
     #[inline]
+    #[must_use]
     pub const fn cell_rect(self, x: i32, y: i32) -> Rect {
         let left = self.cell_size.width * x as f32;
         let top = self.cell_size.height * y as f32;
@@ -131,13 +150,15 @@ impl Grid {
     }
 
     #[inline]
+    #[must_use]
     pub const fn local_to_world(self, pos: Pos) -> Pos {
         let left = self.cell_size.width * pos.x;
         let top = self.cell_size.height * pos.y;
-        Pos::new(left, top)
+        Pos::new(left + self.offset.x, top + self.offset.y)
     }
 
     #[inline]
+    #[must_use]
     pub const fn local_to_world_rect(self, rect: Rect) -> Rect {
         let min = self.local_to_world(rect.min);
         let max = self.local_to_world(rect.max);
@@ -148,13 +169,15 @@ impl Grid {
     }
 
     #[inline]
+    #[must_use]
     pub const fn world_to_local(self, pos: Pos) -> Pos {
-        let left = pos.x / self.cell_size.width;
-        let top = pos.y / self.cell_size.height;
+        let left = (pos.x - self.offset.x) / self.cell_size.width;
+        let top = (pos.y - self.offset.y) / self.cell_size.height;
         Pos::new(left, top)
     }
 
     #[inline]
+    #[must_use]
     pub const fn world_to_local_rect(self, rect: Rect) -> Rect {
         let min = self.world_to_local(rect.min);
         let max = self.world_to_local(rect.max);
@@ -166,6 +189,7 @@ impl Grid {
 
     /// Returns the `(x, y)` coordinate of the cell that `pos` occupies.
     #[inline]
+    #[must_use]
     pub fn cell_coord(self, pos: Pos) -> (i32, i32) {
         let x = (pos.x / self.cell_size.width).floor() as i32;
         let y = (pos.y / self.cell_size.height).floor() as i32;
